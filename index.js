48d3fb60f1cfea9129a3e2c0d51fd040fdf84026
darshan <darshanlila@gmail.com>
2019-08-02 12:57:45 +0530

    removed update endpoint

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ app.get('/ping', (req, res) => {
     res.send('pong');
 });
 
-app.get('/update',(req,res)=>{
-    res.send('updates here');
-});
-
 app.listen(9000,(err)=>{
     if(err){
         console.log(err);
